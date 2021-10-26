bf8ecad8ae9cdc9c071165cc27b6d98618755cd3
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2021-10-26 18:27:53 +0200

    Finish AlphaBetaPruning

--- src/mnkgame/AlphaBetaPruningPlayer.java
+++ src/mnkgame/AlphaBetaPruningPlayer.java
@@ -6,12 +6,6 @@ import java.util.List;
 public class AlphaBetaPruningPlayer implements MNKPlayer {
 	private MNKBoard board;
 	private MNKGameState myWin, yourWin;
-	public class Evaluation {
-		float eval;
-		MNKCell cell;
-		Evaluation(){}
-		Evaluation(Float eval) {this.eval = eval;}
-	}
 	
 	@Override
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
@@ -22,6 +16,7 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 	
 	@Override
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
+		long start = System.currentTimeMillis();
 
 		// Recover the last move
 		if (MC.length > 0) {
@@ -33,57 +28,61 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 		if (FC.length == 1)
 			return FC[0];
 
-		Evaluation eval = alphabeta(board, true, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
-		
-		return eval.cell;
+		MNKCell bestCell = null;
+		float bestEval = Float.NEGATIVE_INFINITY;
+		int depth = 0;
+		for(MNKCell cell: FC) {
+			board.markCell(cell.i, cell.j);
+			float moveEval = alphabeta(board, true, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY, depth);
+			if (moveEval >= bestEval) {
+				bestEval = moveEval;
+				bestCell = cell;
+			}
+			board.unmarkCell();
+		}
+		board.markCell(bestCell.i, bestCell.j);
+
+		System.out.println((System.currentTimeMillis() - start) / 1000.0);
+
+		return bestCell;
 	}
 
-	private Evaluation alphabeta (MNKBoard board, boolean myTurn, float a, float b) {
-		Evaluation eval;
+	private float alphabeta (MNKBoard board, boolean myTurn, float a, float b, int depth) {
 		MNKCell FC[] = board.getFreeCells();
 		
-		if (FC.length ==  1)
-			return evaluate(board, FC[0]);
-		else if (myTurn) {
-			eval = new Evaluation(Float.POSITIVE_INFINITY);
-			for (MNKCell cell : FC) {
-				board.markCell(eval.cell.i, eval.cell.j);
-				Evaluation compareEval = alphabeta(board, false, a, b);
-				board.unmarkCell();
-				if (compareEval.eval < eval.eval)
-					eval = compareEval;
-				b = Math.min(eval.eval, b);
-				if (b <= a)
-					break;
-			}
-		} else {
-			eval = new Evaluation(Float.NEGATIVE_INFINITY);
-			for (MNKCell cell : FC) {
-				board.markCell(eval.cell.i, eval.cell.j);
-				Evaluation compareEval = alphabeta(board, true, a, b);
-				board.unmarkCell();
-				if (compareEval.eval > eval.eval)
-					eval = compareEval;
-				a = Math.max(eval.eval, a);
-				if (b <= a)
-					break;
-			}
-		}
+		if (FC.length ==  0 || board.gameState() != MNKGameState.OPEN)
+			return evaluate(board, depth);
 
-		return eval;
+		if (myTurn) {
+			for (MNKCell cell : FC) {
+				board.markCell(cell.i, cell.j);
+				float eval = alphabeta(board, false, a, b, depth++);
+				board.unmarkCell();
+				b = Math.min(eval, b);
+				if (b <= a)
+					return a;
+			}
+			return b;
+		} else {
+			for (MNKCell cell : FC) {
+				board.markCell(cell.i, cell.j);
+				float eval = alphabeta(board, true, a, b, depth++);
+				board.unmarkCell();
+				a = Math.max(eval, a);
+				if (b <= a)
+					return b;
+			}
+			return a;
+		}
 	}
 
-	private Evaluation evaluate(MNKBoard board, MNKCell cell) {
-		Evaluation e = new Evaluation();
-		MNKGameState state = board.markCell(cell.i, cell.i);
+	private float evaluate(MNKBoard board, int depth) {
+		MNKGameState state = board.gameState();
 		if (state == myWin)
-			e.eval = 1;
+			return 10 - depth;
 		else if (state == yourWin)
-			e.eval = -1;
-		else
-			e.eval = 0;
-		e.cell = cell;
-		return e;
+			return -10 + depth;
+		return 0;
 	}
 
 	@Override
