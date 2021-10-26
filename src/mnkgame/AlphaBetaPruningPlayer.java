6aa4d7d40dcd8c44f3da321569936ffb94b7fa6b
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2021-10-26 15:38:50 +0200

    First implementation of AlphaBetaPruning

--- src/mnkgame/AlphaBetaPruningPlayer.java
+++ src/mnkgame/AlphaBetaPruningPlayer.java
@@ -1,8 +1,17 @@
 package mnkgame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlphaBetaPruningPlayer implements MNKPlayer {
 	private MNKBoard board;
 	private MNKGameState myWin, yourWin;
+	public class Evaluation {
+		float eval;
+		MNKCell cell;
+		Evaluation(){}
+		Evaluation(Float eval) {this.eval = eval;}
+	}
 	
 	@Override
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
@@ -13,41 +22,68 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 	
 	@Override
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
-		return FC[0];
+
+		// Recover the last move
+		if (MC.length > 0) {
+			MNKCell c = MC[MC.length - 1];
+			board.markCell(c.i, c.j);
+		}
+		
+		// Only one possible move
+		if (FC.length == 1)
+			return FC[0];
+
+		Evaluation eval = alphabeta(board, true, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+		
+		return eval.cell;
 	}
 
-	// vedere depth
-	/*
-	ALPHABETA(Tree T, bool mynode, int depth, float a, float b)→ float
-		if(depth = 0 or T.isLeaf())											Controllo FC vuote
-			return EVALUATE(T)
-		elif(mynode = true)
-			eval ← ∞
-			for(Tree c ∈ T.children())										Iter su FC (priorità vittoria mia)
-				eval ← Min(eval,ALPHABETA(c,false,depth-1, a, b))
-				b ← Min(eval, b)
-				if(b ≤ a) // a cutoff
-					break
-				endif
-			endfor
-			return eval
+	private Evaluation alphabeta (MNKBoard board, boolean myTurn, float a, float b) {
+		Evaluation eval;
+		MNKCell FC[] = board.getFreeCells();
+		
+		if (FC.length ==  1)
+			return evaluate(board, FC[0]);
+		else if (myTurn) {
+			eval = new Evaluation(Float.POSITIVE_INFINITY);
+			for (MNKCell cell : FC) {
+				board.markCell(eval.cell.i, eval.cell.j);
+				Evaluation compareEval = alphabeta(board, false, a, b);
+				board.unmarkCell();
+				if (compareEval.eval < eval.eval)
+					eval = compareEval;
+				b = Math.min(eval.eval, b);
+				if (b <= a)
+					break;
+			}
+		} else {
+			eval = new Evaluation(Float.NEGATIVE_INFINITY);
+			for (MNKCell cell : FC) {
+				board.markCell(eval.cell.i, eval.cell.j);
+				Evaluation compareEval = alphabeta(board, true, a, b);
+				board.unmarkCell();
+				if (compareEval.eval > eval.eval)
+					eval = compareEval;
+				a = Math.max(eval.eval, a);
+				if (b <= a)
+					break;
+			}
+		}
+
+		return eval;
+	}
+
+	private Evaluation evaluate(MNKBoard board, MNKCell cell) {
+		Evaluation e = new Evaluation();
+		MNKGameState state = board.markCell(cell.i, cell.i);
+		if (state == myWin)
+			e.eval = 1;
+		else if (state == yourWin)
+			e.eval = -1;
 		else
-			eval ← -∞
-			for(Tree c ∈ T.children())										Iter su FC (priorità vittoria sua)
-				eval ← Max(eval,ALPHABETA(c,true,depth-1, a, b))
-				a ← Max(eval, a)
-				if(b ≤ a) // b cutoff
-					break
-				endif
-			endfor
-			return eval
-		endif
-	*/
-	private void alphabeta (MNKBoard board, boolean myTurn, float a, float b) {
-	}
-
-	private float evaluate(MNKBoard board) {
-		return 0.0F;
+			e.eval = 0;
+		e.cell = cell;
+		return e;
 	}
 
 	@Override
