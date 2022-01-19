943cfcfd9f955781e4f5a2fa44c0d367aa2b4e2a
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-01-19 21:58:27 +0100

    Refactor AIHelper

--- src/mnkgame/AIHelper.java
+++ src/mnkgame/AIHelper.java
@@ -1,14 +1,26 @@
 package mnkgame;
 
-public class AIHelper {
+import java.util.Comparator;
+import java.util.TreeSet;
 
+public class AIHelper {
     static final double LARGE = 100.0;
 
     final int M, N, K;
     final boolean first;
     final int timeout;
     MNKGameState myWin, yourWin;
+    private long start;
+    private Comparator<MNKCellHeuristic> cresc;
+    private Comparator<MNKCellHeuristic> decresc;
 
+    public class MNKCellHeuristic extends MNKCell {
+        double estimate;
+        public MNKCellHeuristic(int i, int j, MNKCellState state, double estimate) {
+            super(i, j, state);
+            this.estimate = estimate;
+        }
+    }
 
     public AIHelper(int M, int N, int K, boolean first, int timeout) {
         this.M = M;
@@ -18,6 +30,34 @@ public class AIHelper {
         this.timeout = timeout;
         myWin = first ? MNKGameState.WINP1 : MNKGameState.WINP2;
         yourWin = first ? MNKGameState.WINP2 : MNKGameState.WINP1;
+        decresc =  (b1, b2) -> {
+            if (b1.estimate - b2.estimate == 0.0) return 1;	// TreeSet non contiene "duplicati"
+            return (int) (b1.estimate - b2.estimate);
+        };
+        cresc =  (b1, b2) -> {
+            if (b2.estimate - b1.estimate == 0.0) return -1;
+            return (int) (b2.estimate - b1.estimate);
+        };
+    }
+
+    public TreeSet<MNKCellHeuristic> getBestMoves(MNKCell FC[], MNKBoard board, boolean myTurn) {
+        TreeSet<MNKCellHeuristic> cells = new TreeSet(myTurn ? decresc : cresc);
+        double estimate;
+        // O(n log n)
+        for (MNKCell cell : FC) {
+            if (isTimeEnded())
+                break;
+            board.markCell(cell.i, cell.j);
+            estimate = evaluate(board, cell);
+            board.unmarkCell();
+            // O(log n)
+            cells.add(new MNKCellHeuristic(cell.i, cell.j, cell.state, estimate));
+            // non ha senso procedere con il calcolo dell'estimate per le altre celle se mi accorgo di essere
+            // in una situazione vantaggiosa/svantaggiosa totale
+            if (Double.isInfinite(estimate))
+                break;
+        }
+        return cells;
     }
 
     public double evaluate(MNKBoard board, MNKCell lastCell) {
@@ -33,7 +73,46 @@ public class AIHelper {
 
     // find threats
 
-    public boolean isTimeEnded(long start) {
+
+    public double alphabeta(MNKBoard board, double estimate, boolean myTurn, double a, double b, int depth) {
+        MNKCell FC[] = board.getFreeCells();
+
+        // situazione vantaggiosa totale
+        if (Double.isInfinite(estimate))
+            return myTurn ? AIHelper.LARGE - depth : -AIHelper.LARGE + depth;
+
+        if (FC.length ==  0 || board.gameState() != MNKGameState.OPEN || isTimeEnded())
+            return estimate;
+
+        TreeSet<MNKCellHeuristic> cells = getBestMoves(FC, board, myTurn);
+        if (myTurn) {
+            for (MNKCellHeuristic cell : cells) {
+                board.markCell(cell.i, cell.j);
+                double eval = alphabeta(board, cell.estimate, false, a, b, depth++);
+                board.unmarkCell();
+                b = Math.min(eval, b);
+                if (b <= a)
+                    return a;
+            }
+            return b;
+        } else {
+            for (MNKCellHeuristic cell : cells) {
+                board.markCell(cell.i, cell.j);
+                double eval = alphabeta(board, cell.estimate, true, a, b, depth++);
+                board.unmarkCell();
+                a = Math.max(eval, a);
+                if (b <= a)
+                    return b;
+            }
+            return a;
+        }
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public boolean isTimeEnded() {
         boolean isTimeEnded = (System.currentTimeMillis()-start) / 1000.0 > timeout*(99.0/100.0);
         return isTimeEnded;
     }
--- src/mnkgame/AlphaBetaPruningPlayer.java
+++ src/mnkgame/AlphaBetaPruningPlayer.java
@@ -1,44 +1,26 @@
 package mnkgame;
 
-import java.util.Comparator;
+
 import java.util.TreeSet;
 
 public class AlphaBetaPruningPlayer implements MNKPlayer {
-	private AIHelper AIHelper;
+	private AIHelper ai;
 	private int M, N, K;
 	private MNKBoard board;
-	private Comparator<MNKCellHeuristic> cresc;
-	private Comparator<MNKCellHeuristic> decresc;
-	private long start;
-
-	private class MNKCellHeuristic extends MNKCell {
-		double estimate;
-		public MNKCellHeuristic(int i, int j, MNKCellState state, double estimate) {
-			super(i, j, state);
-			this.estimate = estimate;
-		}
-	}
 
 	@Override
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
 		this.M = M;
-		this.K = K;
 		this.N = N;
-		AIHelper = new AIHelper(M, N, K, first, timeout_in_secs);
+		this.K = K;
+		ai = new AIHelper(M, N, K, first, timeout_in_secs);
 		board = new MNKBoard(M, N, K);
-		decresc =  (b1, b2) -> {
-			if (b1.estimate - b2.estimate == 0.0) return 1;	// TreeSet non contiene "duplicati"
-			return (int) (b1.estimate - b2.estimate);
-		};
-		cresc =  (b1, b2) -> {
-			if (b2.estimate - b1.estimate == 0.0) return -1;
-			return (int) (b2.estimate - b1.estimate);
-		};
 	}
 
 	@Override
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
-		start = System.currentTimeMillis();
+		long start = System.currentTimeMillis();
+		ai.setStart(start);
 
 		if (MC.length > 0) {
 			// Recover the last move
@@ -59,12 +41,12 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 		MNKCell bestCell = null;
 		double bestEval = Double.NEGATIVE_INFINITY;
 		int depth = 0;
-		TreeSet<MNKCellHeuristic> cells = getBestMoves(FC, board, true);
-		for(MNKCellHeuristic cell: cells) {
-			if (AIHelper.isTimeEnded(start)) break;
+		TreeSet<AIHelper.MNKCellHeuristic> cells = ai.getBestMoves(FC, board, true);
+		for(AIHelper.MNKCellHeuristic cell: cells) {
+			if (ai.isTimeEnded()) break;
 
 			board.markCell(cell.i, cell.j);
-			double moveEval = alphabeta(board, cell.estimate, true, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, depth);
+			double moveEval = ai.alphabeta(board, cell.estimate, true, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, depth);
 			if (moveEval >= bestEval) {
 				bestEval = moveEval;
 				bestCell = cell;
@@ -76,60 +58,6 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 		System.out.println(System.currentTimeMillis() - start);
 
 		return bestCell;
-	}
-
-	private double alphabeta(MNKBoard board, double estimate, boolean myTurn, double a, double b, int depth) {
-		MNKCell FC[] = board.getFreeCells();
-
-		// situazione vantaggiosa totale
-		if (Double.isInfinite(estimate))
-			return myTurn ? AIHelper.LARGE - depth : -AIHelper.LARGE + depth;
-
-		if (FC.length ==  0 || board.gameState() != MNKGameState.OPEN || AIHelper.isTimeEnded(start))
-			return estimate;
-
-		TreeSet<MNKCellHeuristic> cells = getBestMoves(FC, board, myTurn);
-		if (myTurn) {
-			for (MNKCellHeuristic cell : cells) {
-				board.markCell(cell.i, cell.j);
-				double eval = alphabeta(board, cell.estimate, false, a, b, depth++);
-				board.unmarkCell();
-				b = Math.min(eval, b);
-				if (b <= a)
-					return a;
-			}
-			return b;
-		} else {
-			for (MNKCellHeuristic cell : cells) {
-				board.markCell(cell.i, cell.j);
-				double eval = alphabeta(board, cell.estimate, true, a, b, depth++);
-				board.unmarkCell();
-				a = Math.max(eval, a);
-				if (b <= a)
-					return b;
-			}
-			return a;
-		}
-	}
-
-	TreeSet<MNKCellHeuristic> getBestMoves(MNKCell FC[], MNKBoard board, boolean myTurn) {
-		TreeSet<MNKCellHeuristic> cells = new TreeSet(myTurn ? decresc : cresc);
-		double estimate;
-		// O(n log n)
-		for (MNKCell cell : FC) {
-			if (AIHelper.isTimeEnded(start)) break;
-
-			board.markCell(cell.i, cell.j);
-			estimate = AIHelper.evaluate(board, cell);
-			board.unmarkCell();
-			// O(log n)
-			cells.add(new MNKCellHeuristic(cell.i, cell.j, cell.state, estimate));
-			// non ha senso procedere con il calcolo dell'estimate per le altre celle se mi accorgo di essere
-			// in una situazione vantaggiosa/svantaggiosa totale
-			if (Double.isInfinite(estimate))
-				break;
-		}
-		return cells;
 	}
 
 	@Override
