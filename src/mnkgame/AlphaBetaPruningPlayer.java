015eecd4fa2d41dba147cac63c464ecc02a13c39
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-01-19 18:25:12 +0100

    Setup pre euristiche su evaluate

--- src/mnkgame/AlphaBetaPruningPlayer.java
+++ src/mnkgame/AlphaBetaPruningPlayer.java
@@ -6,30 +6,28 @@ import java.util.TreeSet;
 public class AlphaBetaPruningPlayer implements MNKPlayer {
 	private Heuristics heuristics;
 	private MNKBoard board;
-	private MNKGameState myWin, yourWin;
 	private Comparator<MNKCellHeuristic> cresc;
 	private Comparator<MNKCellHeuristic> decresc;
 
 	private class MNKCellHeuristic extends MNKCell {
-		private float estimate;
-		public MNKCellHeuristic(int i, int j, MNKCellState state, float estimate) { super(i, j, state); this.estimate = estimate; }
-		public float getEstimate() { return estimate; }
-		public void setEstimate(float estimate) { this.estimate = estimate; }
+		double estimate;
+		public MNKCellHeuristic(int i, int j, MNKCellState state, double estimate) {
+			super(i, j, state);
+			this.estimate = estimate;
+		}
 	}
 
 	@Override
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
-		heuristics = new Heuristics();
+		heuristics = new Heuristics(M, N, K, first, timeout_in_secs);
 		board = new MNKBoard(M, N, K);
-		myWin = first ? MNKGameState.WINP1 : MNKGameState.WINP2;
-		yourWin = first ? MNKGameState.WINP2 : MNKGameState.WINP1;
 		decresc =  (b1, b2) -> {
-			if (b1.getEstimate() - b2.getEstimate() == 0.0) return 1;
-			return (int) (b1.getEstimate() - b2.getEstimate());
+			if (b1.estimate - b2.estimate == 0.0) return 1;	// TreeSet non contiene "duplicati"
+			return (int) (b1.estimate - b2.estimate);
 		};
 		cresc =  (b1, b2) -> {
-			if (b2.getEstimate() - b1.getEstimate() == 0.0) return -1;
-			return (int) (b2.getEstimate() - b1.getEstimate());
+			if (b2.estimate - b1.estimate == 0.0) return -1;
+			return (int) (b2.estimate - b1.estimate);
 		};
 	}
 
@@ -49,12 +47,12 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 
 		// ALPHABETA
 		MNKCell bestCell = null;
-		float bestEval = Float.NEGATIVE_INFINITY;
+		double bestEval = Double.NEGATIVE_INFINITY;
 		int depth = 0;
 		TreeSet<MNKCellHeuristic> cells = getNextMoves(FC, board, true);
-		for(MNKCell cell: cells) {
+		for(MNKCellHeuristic cell: cells) {
 			board.markCell(cell.i, cell.j);
-			float moveEval = alphabeta(board, true, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY, depth);
+			double moveEval = alphabeta(board, cell.estimate, true, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, depth);
 			if (moveEval >= bestEval) {
 				bestEval = moveEval;
 				bestCell = cell;
@@ -68,26 +66,22 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 		return bestCell;
 	}
 
-	private float alphabeta(MNKBoard board, boolean myTurn, float a, float b, int depth) {
+	private double alphabeta(MNKBoard board, double estimate, boolean myTurn, double a, double b, int depth) {
 		MNKCell FC[] = board.getFreeCells();
 
-		// float estimate = (Valutazione tramite euristiche sulla board e celle libere)
+		// situazione vantaggiosa/svantaggiosa totale
+		if (Double.isInfinite(estimate))
+			return myTurn ? heuristics.LARGE - depth : -heuristics.LARGE + depth;
 
-		/*
-		if (estimate +- inf)
-			situazione vantaggiosa/svantaggiosa totale
-			return LARGE - depth;
-		 */
-
+//		 TODO: add condizione di tempo scaduto
 		if (depth == 2 || FC.length ==  0 || board.gameState() != MNKGameState.OPEN)
-			return evaluate(board, depth);	// return estimate
+			return estimate;
 
 		TreeSet<MNKCellHeuristic> cells = getNextMoves(FC, board, myTurn);
 		if (myTurn) {
-			// O(n log n)
 			for (MNKCellHeuristic cell : cells) {
 				board.markCell(cell.i, cell.j);
-				float eval = alphabeta(board, false, a, b, depth++);
+				double eval = alphabeta(board, cell.estimate, false, a, b, depth++);
 				board.unmarkCell();
 				b = Math.min(eval, b);
 				if (b <= a)
@@ -97,7 +91,7 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 		} else {
 			for (MNKCellHeuristic cell : cells) {
 				board.markCell(cell.i, cell.j);
-				float eval = alphabeta(board, true, a, b, depth++);
+				double eval = alphabeta(board, cell.estimate, true, a, b, depth++);
 				board.unmarkCell();
 				a = Math.max(eval, a);
 				if (b <= a)
@@ -108,28 +102,21 @@ public class AlphaBetaPruningPlayer implements MNKPlayer {
 	}
 
 	TreeSet<MNKCellHeuristic> getNextMoves(MNKCell FC[], MNKBoard board, boolean isDecresc) {
-		TreeSet cells = new TreeSet(isDecresc ? cresc : decresc);
-		float estimate;
+		TreeSet<MNKCellHeuristic> cells = new TreeSet(isDecresc ? decresc : cresc);
+		double estimate;
 		// O(n log n)
 		for (MNKCell cell : FC) {
 			board.markCell(cell.i, cell.j);
-			// estimate = heuristics.estimate(board)
-			estimate = 0;
+			estimate = heuristics.evaluate(board, cell);
 			board.unmarkCell();
 			// O(log n)
 			cells.add(new MNKCellHeuristic(cell.i, cell.j, cell.state, estimate));
+			// non ha senso procedere con il calcolo dell'estimate per le altre celle se mi accorgo di essere
+			// in una situazione vantaggiosa/svantaggiosa totale
+			if (Double.isInfinite(estimate))
+				break;
 		}
 		return cells;
-	}
-
-
-	private float evaluate(MNKBoard board, int depth) {
-		MNKGameState state = board.gameState();
-		if (state == myWin)
-			return 10 - depth;
-		else if (state == yourWin)
-			return -10 + depth;
-		return 0;
 	}
 
 	@Override
--- src/mnkgame/Heuristics.java
+++ src/mnkgame/Heuristics.java
@@ -2,7 +2,32 @@ package mnkgame;
 
 public class Heuristics {
 
-    public Heuristics(){}
+    static final double LARGE = 100.0;
+
+    private int M, N, K;
+    private boolean first;
+    private int timeout_in_secs;
+    private MNKGameState myWin, yourWin;
+
+
+    public Heuristics(int M, int N, int K, boolean first, int timeout_in_secs) {
+        this.M = M;
+        this.N = N;
+        this.K = K;
+        this.first = first;
+        this.timeout_in_secs = timeout_in_secs;
+        myWin = first ? MNKGameState.WINP1 : MNKGameState.WINP2;
+        yourWin = first ? MNKGameState.WINP2 : MNKGameState.WINP1;
+    }
+
+    public double evaluate(MNKBoard board, MNKCell lastCell) {
+        MNKGameState state = board.gameState();
+        if (state == myWin || state == yourWin)
+            return Double.POSITIVE_INFINITY;
+        return 0;
+        // qui euristiche su partita ancora aperta
+
+    }
 
 
 }
